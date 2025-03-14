89488d3419439e8968b8e1bb722e3add8ce29917
parisan <parisan-0487.com>
2025-03-14 16:11:55 +0330

    footer

--- src/components/layout/footer/footerComp.tsx
+++ src/components/layout/footer/footerComp.tsx
@@ -17,17 +17,18 @@ export default function FooterComp() {
   return (
     <div>
       <div className="relative w-full h-auto p-10 bg-purple-300 overflow-hidden font-gandom">
-        <div className="absolute top-[-150px] left-[-200px] w-[500px] h-[500px] bg-gradient-to-br from-[#f3e8ff] via-[#f7d1ff] to-[#f8f9ff] rounded-full opacity-40"></div>
-        <div className="absolute bottom-[-150px] right-[-200px] w-[500px] h-[500px] bg-gradient-to-br from-[#f3e8ff] via-[#f7d1ff] to-[#f8f9ff] rounded-full opacity-40"></div>
+        {/* افکت‌های بک‌گراند */}
+        <div className="absolute top-[-150px] left-[-200px] w-[500px] h-[500px] bg-gradient-to-br from-[#f3e8ff] via-[#f7d1ff] to-[#f8f9ff] rounded-full opacity-40 z-0"></div>
+        <div className="absolute bottom-[-150px] right-[-200px] w-[500px] h-[500px] bg-gradient-to-br from-[#f3e8ff] via-[#f7d1ff] to-[#f8f9ff] rounded-full opacity-40 z-0"></div>
 
-        {/* شبکه بندی فوتر */}
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-20 text-right">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-20 text-right">
           <motion.div
             variants={fadeInFromLeft}
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: true }}
-            custom={0} // اولین ستون
+            viewport={{ once: false, amount: 0.5 }}
+            custom={0}
+            className="z-10"
           >
             {/* نماد */}
           </motion.div>
@@ -36,11 +37,12 @@ export default function FooterComp() {
             variants={fadeInFromLeft}
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: true }}
-            custom={1} // دومین ستون
+            viewport={{ once: false, amount: 0.5 }}
+            custom={1}
+            className="flex flex-col items-end gap-3 z-10"
           >
-            <h3 className="text-lg font-semibold mb-3">دسته‌بندی‌ها</h3>
-            <ul className="space-y-2 mt-[30px]">
+            <h3 className="text-lg md:text-xl font-semibold">دسته‌بندی‌ها</h3>
+            <ul className="space-y-2">
               <li>
                 <Link href="/boy">
                   <span className="hover:text-gray-700 font-bold">پسرانه</span>
@@ -58,11 +60,12 @@ export default function FooterComp() {
             variants={fadeInFromLeft}
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: true }}
-            custom={2} // سومین ستون
+            viewport={{ once: false, amount: 0.5 }}
+            custom={2}
+            className="flex flex-col items-end gap-3 z-10"
           >
-            <h3 className="text-lg font-semibold mb-3">لینک‌های مفید</h3>
-            <ul className="space-y-2 mt-[30px]">
+            <h3 className="text-lg md:text-xl font-semibold">لینک‌های مفید</h3>
+            <ul className="space-y-2">
               <li>
                 <Link href="/contact-us">
                   <span className="hover:text-gray-700 font-bold">
@@ -91,51 +94,60 @@ export default function FooterComp() {
             variants={fadeInFromLeft}
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: true }}
-            custom={3} // چهارمین ستون
+            viewport={{ once: false, amount: 0.5 }}
+            custom={3}
+            className="flex flex-col items-end gap-4 z-10"
           >
-            <h2 className="font-bold text-xl opacity-90 mb-4 mr-[17px]">
+            <h2 className="font-bold text-xl md:text-2xl opacity-90">
               ارتباط با شادینا
             </h2>
-            <div className="flex items-center font-bold opacity-90 ml-[59px]">
-              <span>کاشمر، بین مدرس ۲-۴ </span>
+
+            <div className="flex items-center gap-2">
               <Image
                 src="/img/location.png"
                 alt="Location"
                 width={30}
                 height={30}
-                className="opacity-90 m-3"
+                className="z-10"
               />
+              <span className="font-bold opacity-90 z-10">
+                کاشمر، بین مدرس ۲-۴
+              </span>
             </div>
-            <div className="flex items-center font-bold opacity-90 ml-[88px]">
-              <span>shadina_shop1</span>
+
+            <div className="flex items-center gap-2">
               <Image
                 src="/img/instagram.png"
                 alt="Instagram"
                 width={30}
                 height={30}
-                className="opacity-90 m-3"
+                className="z-10"
               />
+              <span className="font-bold opacity-90 z-10">shadina_shop1</span>
             </div>
-            <div className="flex items-center font-bold opacity-90 ml-[94px]">
-              <span>0905 186 7829</span>
+
+            <div className="flex items-center gap-2">
               <Image
                 src="/img/call.png"
                 alt="Call"
                 width={30}
                 height={30}
-                className="opacity-90 m-3"
+                className="z-10"
               />
+              <span className="font-bold opacity-90 z-10">0905 186 7829</span>
             </div>
-            <div className="flex items-center font-bold opacity-90 ml-[-3px]">
-              <span>shadina_shop1@gmail.com</span>
+
+            <div className="flex items-center gap-2">
               <Image
                 src="/img/email.png"
                 alt="Email"
                 width={30}
                 height={30}
-                className="opacity-90 m-3"
+                className="z-10"
               />
+              <span className="font-bold opacity-90 z-10">
+                shadina_shop1@gmail.com
+              </span>
             </div>
           </motion.div>
         </div>
