66711cbc1b2b1d6cb969515f0f55d48fd075c33e
parisan <parisan-0487.com>
2025-03-14 15:39:15 +0330

    edit

--- src/components/slider/girlBody.tsx
+++ src/components/slider/girlBody.tsx
@@ -45,7 +45,7 @@ export default function GirlBody() {
   return (
     <div className="max-w-[80rem] h-[25rem] mx-auto mb-8 p-8 rounded-2xl bg-gray-100 shadow-[inset_8px_8px_16px_#bebebe,inset_-8px_-8px_16px_#ffffff,8px_8px_16px_rgba(0,0,0,0.1),-8px_-8px_16px_rgba(255,255,255,0.7)] relative overflow-hidden">
       {loading ? (
-        <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center justify-center min-h-screen mt-[-13rem]">
           <p className="text-center text-gray-500">در حال دریافت محصولات</p>
           <LoadingSpinner />
         </div>
