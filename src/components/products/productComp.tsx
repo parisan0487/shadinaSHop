393a2f32c7b0c98006f658ee2c6cfbfa37ac1268
parisan <parisan-0487.com>
2025-03-14 20:35:26 +0330

    edit

--- src/components/products/productComp.tsx
+++ src/components/products/productComp.tsx
@@ -370,7 +370,7 @@ export default function ProductComp() {
                   width={40}
                   height={40}
                   className="w-full h-full object-cover"
-                  id="icon"
+                  id="icon-pro"
                 />
               </div>
               <span
