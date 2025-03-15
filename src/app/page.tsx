bc3f45074476b1c0569b08e3e5991fefb79a768a
parisan <parisan-0487.com>
2025-03-15 17:10:16 +0330

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import BodyComp from "../components/layout/body/bodyComp";
 import HeaderComp from "../components/layout/header/headerComp";
 import Footer from "../components/layout/footer/footerComp";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>shadinaShop</title>
-        <meta name="description" content="پوشاک بچه گانه شادینا" />
-      </Head>
       <HeaderComp />
       <BodyComp />
       <Footer />
